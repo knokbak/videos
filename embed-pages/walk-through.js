a7366655e0203a9427e42bc7721d8ebe3a4b9d67
Ollie <ollie@auntodev.com>
2020-12-29 23:36:16 +0000

    Update walk-through.js

--- embed-pages/walk-through.js
+++ embed-pages/walk-through.js
@@ -1,51 +1,107 @@
+/*
+
+    This is free and unencumbered software released into the public domain.
+
+    Anyone is free to copy, modify, publish, use, compile, sell, or
+    distribute this software, either in source code form or as a compiled
+    binary, for any purpose, commercial or non-commercial, and by any
+    means.
+
+*/
+
+
 // First, we'll import Discord.JS.
 const Discord = require('discord.js');
 
-// CODE:
-let current = 1;
-let m = await msg.channel.send('Loading pages...');
-showPage(msg, current);
-
-async function showPage (message, page) {
-    let output = createEmbed(page);
-    await m.edit(null, { embed: output });
-    await m.reactions.removeAll();
-
-    let left, right;
-
-    if (output[1]) {
-        await m.react('⬅️');
-
-        let filter = (r, u) => r.emoji.name == '⬅️' && u.id == message.author.id;
-        left = m.createReactionCollector(filter, { time: 60000 });
-        left.on('collect', r => {
-            m.reactions.removeAll();
-
-            if (right) right.stop();
-            left.stop();
-
-            showPage(current - 1);
-        });
+// Now we'll go ahead and create the command to be registered by our command handler.
+exports.run = async(client, msg, args) => {
+    // Now, we need some content to actually post so we'll create an ARRAY with our pages.
+    let pages = [
+        'This is our first page...',
+        '...this is our second...',
+        '..and this is our third!'
+    ];
+    
+    // We'll now create a place to store the current page number...
+    let current = 1;
+    
+    // ...and will now post a loading message which we will edit later.
+    let m = await msg.channel.send('Loading pages...');
+    
+    // We'll now create a function to create embed pages.
+    function createEmbed (page) {
+        // We'll just make the embed and return it.
+        let embed = new Discord.MessageEmbed()
+        .setDescription(pages[page]);
+        return embed;
     };
-
-    if (output[2]) {
-        await m.react('➡️');
-
-        let filter = (r, u) => r.emoji.name == '➡️' && u.id == message.author.id;
-        right = m.createReactionCollector(filter, { time: 60000 });
-        right.on('collect', r => {
-            m.reactions.removeAll();
-
-            if (left) left.stop();
-            right.stop();
-
-            showPage(current + 1);
-        });
+    
+    // We'll also create another function to determine whether we need to show reactions.
+    function reactionsNeeded (page) {
+        // We'll now return an ARRAY with the results.
+        // The first element will be backwards, second will be forwards.
+        return [
+            pages[page - 1],
+            pages[page + 1]
+        ];
     };
-};
-
-function createEmbed (page) {
-    let embed = new Discord.MessageEmbed()
-    .setDescription(content[page]);
-    return embed;
+    
+    // Next, we'll make another function which will be used to show pages.
+    async function showPage (page) {
+        // We'll quickly use our createEmbed function to get the page we need...
+        let output = createEmbed(page);
+        
+        // ...and then we'll edit our original message with the new embed.
+        // We set the message content to NULL to remove the original loading message and then attach our embed to the message.
+        await m.edit(null, { embed: output });
+        
+        // To make sure our reaction system works, we'll remove all previous reactions, if there is any.
+        await m.reactions.removeAll();
+        
+        // We'll create a couple variables to store our reaction listeners in and our reactionNeeded result.
+        let needed = reactionsNeeded(page);
+        let left, right;
+        
+        // If we actually need to add a backwards reaction, we will.
+        if (needed[0]) {
+            // As it's needed, we'll add the reaction.
+            await m.react('⬅️');
+            
+            // We'll quickly create a reaction collector filter so we only collect the right events...
+            let filter = (r, u) => r.emoji.name == '⬅️' && u.id == message.author.id;
+            
+            // ...and then set the variable we made earlier. We'll make sure our collector times out after 60000ms (60 seconds).
+            left = m.createReactionCollector(filter, { time: 60000 });
+            
+            // We'll now handle the add reaction event.
+            left.on('collect', r => {
+                // We'll stop listening for any more reactions here...
+                if (right) right.stop();
+                left.stop();
+                
+                // ...and will then show the new page and update the current page.
+                showPage(current - 1);
+                current = current - 1;
+            });
+        };
+        
+        // We'll now do that again for the other reaction. Only minor changes are made.
+        if (needed[1]) {
+            await m.react('➡️');
+            
+            let filter = (r, u) => r.emoji.name == '➡️' && u.id == message.author.id;
+            left = m.createReactionCollector(filter, { time: 60000 });
+            
+            left.on('collect', r => {
+                if (left) left.stop();
+                right.stop();
+                
+                showPage(current + 1);
+                current = current + 1;
+            });
+        };
+    };
+    
+    // Now we've done that, we will now create our page system.
+    showPage(current);
 };
