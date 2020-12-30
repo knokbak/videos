1c7de4f41b8ca857c36888e74d92cd35791778c1
Ollie <ollie@auntodev.com>
2020-12-30 01:46:21 +0000

    Update index.js

--- embed-pages/index.js
+++ embed-pages/index.js
@@ -1,51 +1,68 @@
-// IMPORTS:
+// ⬅️ ➡️
+
 const Discord = require('discord.js');
 
-// CODE:
-let current = 1;
-let m = await msg.channel.send('Loading pages...');
-showPage(msg, current);
+exports.run = async(client, msg, args) => {
+    let pages = [
+        'Page 1', // 0
+        'Page 2', // 1
+        'Page 3' // 2
+    ];
 
-async function showPage (message, page) {
-    let output = createEmbed(page);
-    await m.edit(null, { embed: output });
-    await m.reactions.removeAll();
+    let current = 0;
+    let m = await msg.channel.send('Loading pages...');
 
-    let left, right;
-
-    if (output[1]) {
-        await m.react('⬅️');
-
-        let filter = (r, u) => r.emoji.name == '⬅️' && u.id == message.author.id;
-        left = m.createReactionCollector(filter, { time: 60000 });
-        left.on('collect', r => {
-            m.reactions.removeAll();
-
-            if (right) right.stop();
-            left.stop();
-
-            showPage(current - 1);
-        });
+    function createEmbed (page) {
+        let embed = new Discord.MessageEmbed()
+        .setDescription(pages[page]);
+        return embed;
     };
 
-    if (output[2]) {
-        await m.react('➡️');
-
-        let filter = (r, u) => r.emoji.name == '➡️' && u.id == message.author.id;
-        right = m.createReactionCollector(filter, { time: 60000 });
-        right.on('collect', r => {
-            m.reactions.removeAll();
-
-            if (left) left.stop();
-            right.stop();
-
-            showPage(current + 1);
-        });
+    function reactionsNeeded (page) {
+        return [
+            pages[page - 1],
+            pages[page + 1]
+        ];
     };
-};
 
-function createEmbed (page) {
-    let embed = new Discord.MessageEmbed()
-    .setDescription(content[page]);
-    return embed;
+    async function showPage (page) {
+        let output = createEmbed(page);
+        await m.edit(null, { embed: output });
+        await m.reactions.removeAll();
+
+        let needed = reactionsNeeded(page);
+        let left, right;
+
+        if (needed[0]) {
+            await m.react('⬅️');
+
+            let filter = (r, u) => r.emoji.name == '⬅️' && u.id == msg.author.id;
+            left = m.createReactionCollector(filter, { time: 60000 });
+
+            left.on('collect', (r) => {
+                if (right) right.stop();
+                left.stop();
+
+                showPage(current - 1);
+                current = current - 1;
+            });
+        };
+
+        if (needed[1]) {
+            await m.react('➡️');
+
+            let filter = (r, u) => r.emoji.name == '➡️' && u.id == msg.author.id;
+            right = m.createReactionCollector(filter, { time: 60000 });
+
+            right.on('collect', (r) => {
+                if (left) left.stop();
+                right.stop();
+
+                showPage(current + 1);
+                current = current + 1;
+            });
+        };
+    };
+
+    showPage(current);
 };
